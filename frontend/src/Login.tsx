df82b63d10a3db7d60a95e6b197959b042dc3804
tbr0117 <bhargavreddy033@gmail.com>
2022-01-04 15:52:56 +0530

    simple login

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
+import axios from "axios";
 
 import {FlexBox, FlexBoxDirection, Button,
         FlexBoxJustifyContent, Input, InputType, Label, FlexBoxAlignItems} from "@ui5/webcomponents-react";
@@ -11,14 +11,35 @@ function Login() {
         direction: FlexBoxDirection.Column,
         alignItems: FlexBoxAlignItems.Center,
         style: { "width": "100%" }
-}
+    }
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    async function apicall(params:any) {
+      return axios.post("http://localhost:8000/login", params)
+    }
+    async function loginUser(){
+        debugger;
+        const params = new URLSearchParams();
+        params.append("username", userName);
+        params.append("password", password);
+        const response = await apicall(params);
+    };
     return (
         <FlexBox {...aflexBoxProperties}>
             <Label for="UserName" > User Name </Label>
-            <Input id="UserName" type={InputType.Text} />
+            <Input id="UserName" onInput={(a)=>{ 
+                if(!a || !a.srcElement) {
+                    return
+                }
+                setUserName(a.srcElement["getAttribute"]("value"))}} type={InputType.Text} />
             <Label for ="Password">Password</Label>
-            <Input id="Password" type={InputType.Password}></Input>
-            <Button icon="employee">Login</Button>
+            <Input id="Password" type={InputType.Password} 
+             onInput={(a)=>{ 
+                if(!a || !a.srcElement) {
+                    return
+                }
+                setPassword(a.srcElement["getAttribute"]("value"))}}></Input>
+            <Button icon="employee" onClick={loginUser}>Login</Button>
         </FlexBox>  
     )
 }
